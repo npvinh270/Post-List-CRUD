75946c900e842581d39261cf9ab6538bc8c1e0d0
Lap trinh Django <npvinh270@gmail.com>
2021-09-21 14:28:37 +0700

    ok

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 # blog/views.py
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView,UpdateView
 from django.shortcuts import render
 from .models import Post
 
@@ -19,3 +19,9 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = ['title', 'author', 'body']
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
